24711a240a0a3564fa5facf7d905c9e41b5e8ca9
Shinji Matsumoto <smatsumoto@ciaoinc.jp>
2019-08-17 22:55:00 +0900

    Add "load_env" and "aggregare_input_func" functions.

--- smutil/smutil.py
+++ smutil/smutil.py
@@ -1,5 +1,62 @@
 # -*- coding: utf-8 -*-
 
+"""
+snakemake utils
+"""
+
+import os
+
+import configargparse
+from snakemake import checkpoints
+from snakemake.io import expand, glob_wildcards
+
+
+def load_env(env_file: str = '.env'):
+    """
+    load .env file, and set as environment variables
+    :param env_file: env filename (default ".env")
+    """
+    env_conf = configargparse.DefaultConfigFileParser()
+    with open(env_file) as conf_file:
+        envs = env_conf.parse(conf_file)
+    envs.update(os.environ)  # don't overwrite original environment
+    os.environ.update(envs)
+
+
+def aggregate_input_func(base_checkpoint, base_rule, target_rule=None, output_key=0):
+    """
+    return input function for aggregating inputs from checkpoint
+    :param base_checkpoint: checkpoint name
+    :param base_rule: aggregation filenames at checkpoint
+    :param target_rule: aggregation filenames at previous rule. If previous rule is base_checkpoint, set None (default: None)
+    :param output_key: key of checkpoint's output for aggregation directory. (default: 0)
+    :return: function for input section
+    """
+    def aggregate_input(wildcards):
+        ops = vars(checkpoints).get(base_checkpoint).get(**wildcards).output
+        checkpoint_output = _output_accessor(ops, output_key)
+        expand_base_rule = os.path.join(checkpoint_output, base_rule)
+        expand_target_rule = target_rule or expand_base_rule
+        return expand(expand_target_rule,
+                      **glob_wildcards(expand_base_rule)._asdict())
+    return aggregate_input
+
+
+def _output_accessor(output, output_key):
+    """
+    access output member with output_key(int, str or callable)
+    :param output:
+    :param output_key:
+    :return:
+    """
+    if isinstance(output_key, int):
+        return output[output_key]
+    elif isinstance(output_key, str):
+        return getattr(output, output_key)
+    else:
+        # assume callable
+        return output_key(output)
+
 
 def print_help(rules):
     """
