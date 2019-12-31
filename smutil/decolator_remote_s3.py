cb41dbf294f6d230bcaaffa41b32eb4e63927b21
Shinji Matsumoto <smatsumoto@ciaoinc.jp>
2019-12-31 21:17:49 +0900

    Add remote_s3 decorator, which wraps input and output with S3.remote()

--- smutil/decolator_remote_s3.py
+++ smutil/decolator_remote_s3.py
@@ -17,14 +17,33 @@ rule some_rule:
 
 """
 
+import os
+
 from snakemake.remote.S3 import RemoteProvider as S3RemoteProvider
 S3 = S3RemoteProvider()
 
 
-def remote_s3(disable=False, **s3_remote_params):
+def s3_remote_wrapper(disable=False, bucket='', *args, **s3_remote_params):
+    """
+
+    :param bucket:
+    :param args:
+    :param s3_remote_params:
+    :return:
+    """
+    if disable:
+        return args  # if "disable" = True, do nothing
+    # if expand() is applied, the item in args is list, otherwise it is str.
+    args_with_bucket = [[os.path.join(bucket, i) for i in x] if isinstance(x, list) else os.path.join(bucket, x)
+                        for x in args]
+    return S3.remote(*args_with_bucket, **s3_remote_params)
+
+
+def remote_s3(disable=False, bucket='', **s3_remote_params):
     """
     Decorator for snakemake 'rule:'. Wrap "input:" and "output:" with S3.remote() call
     :param disable: Disable S3.remote wrapping. Default: False
+    :param bucket: bucket name automatically added. Default: '' (no buckect name added, assumed "top folder" is bucket name same as S3.
     :param s3_remote_params: parameters passed for S3.remote()
     :return: Rule decorator
     """
@@ -32,17 +51,13 @@ def remote_s3(disable=False, **s3_remote_params):
         if disable:
             return ruleinfo  # if "disable" = True, do nothing
 
-        # Apply S3.remote(*. keep_local=True) for all non-keywords of input and output.
-        def s3_remote_(*args):
-            return S3.remote(*args, **s3_remote_params)
-
         # Wrap 'input:' and 'output:' with S3.remote call
         if ruleinfo.input:
-            ruleinfo.input = (tuple([s3_remote_(x) for x in ruleinfo.input[0]]),
-                              {k: s3_remote_(v) for k, v in ruleinfo.input[1].items()})
+            ruleinfo.input = (tuple([s3_remote_wrapper(bucket, x, **s3_remote_params) for x in ruleinfo.input[0]]),
+                              {k: s3_remote_wrapper(bucket, v, **s3_remote_params) for k, v in ruleinfo.input[1].items()})
         if ruleinfo.output:
-            ruleinfo.output = (tuple([s3_remote_(x) for x in ruleinfo.output[0]]),
-                               {k: s3_remote_(v) for k, v in ruleinfo.output[1].items()})
+            ruleinfo.output = (tuple([s3_remote_wrapper(bucket, x, **s3_remote_params) for x in ruleinfo.output[0]]),
+                               {k: s3_remote_wrapper(bucket, v, **s3_remote_params) for k, v in ruleinfo.output[1].items()})
 
         return ruleinfo
 
